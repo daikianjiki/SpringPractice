ec3bb57c6f909ebec9405c11055c48741e73d1d5
Daiki Anjiki <daikianjiki@gmail.com>
2023-08-21 08:43:40 -0700

    practiced creating beans using xml

---
+++ src/main/java/demo/Doctor.java
@@ -0,0 +1,33 @@
+package demo;
+
+public class Doctor implements Staff{
+
+    private String qualification;
+
+    public Doctor(String qualification) {
+        this.qualification = qualification;
+    }
+
+//    private Nurse nurse;
+
+
+    public void assist() {
+        System.out.println("Doctor is assisting");
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+//    public Nurse getNurse() {
+//        return nurse;
+//    }
+//
+//    public void setNurse(Nurse nurse) {
+//        this.nurse = nurse;
+//    }
+}
